b791cc07e3f14479233b93464e895d193e3da2fd
mournfully <minuramolligoda.dev@gmail.com>
2022-11-10 23:24:44 -0500

    added README and working directory

--- main.tf
+++ main.tf
@@ -12,33 +12,45 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
+# create infrastructure modules
 module "network_1" {
   source = "./modules/network"
 }
 
-module "loadbalancer_1" {
+module "testbench_1" {
   source = "./modules/virtual-machine"
   # set variables for module
-  vm_name = "loadbalancer_1"
+  vm_name = "testbench_1"
   net_name = "network_1"
-  mac_addr = "52:54:00:6c:3c:02"
-  cloudinit_path = "${path.module}/cloud_init_lb.cfg"
+  mac_addr = "52:54:00:6c:3c:02" 
+  cloudinit_path = "${path.module}/cloud_init_test.cfg"
 }
 
-module "node_1" {
-  source = "./modules/virtual-machine"
-  # set variables for module
-  vm_name = "node_1"
-  net_name = "network_1"
-  mac_addr = "52:54:00:6c:3c:03"
-  cloudinit_path = "${path.module}/cloud_init_vm.cfg"
-}
+# module "loadbalancer_1" {
+#   source = "./modules/virtual-machine"
+#   # set variables for module
+#   vm_name = "loadbalancer_1"
+#   net_name = "network_1"
+#   mac_addr = "52:54:00:6c:3c:02"
+#   cloudinit_path = "${path.module}/cloud_init_lb.cfg"
+# }
 
-module "node_2" {
-  source = "./modules/virtual-machine"
-  # set variables for module
-  vm_name = "node_2"
-  net_name = "network_1"
-  mac_addr = "52:54:00:6c:3c:04"
-  cloudinit_path = "${path.module}/cloud_init_vm.cfg"
-}
+# module "node_1" {
+#   source = "./modules/virtual-machine"
+#   # set variables for module
+#   vm_name = "node_1"
+#   net_name = "network_1"
+#   mac_addr = "52:54:00:6c:3c:03"
+#   cloudinit_path = "${path.module}/cloud_init_vm.cfg"
+# }
+
+# module "node_2" {
+#   source = "./modules/virtual-machine"
+#   # set variables for module
+#   vm_name = "node_2"
+#   net_name = "network_1"
+#   mac_addr = "52:54:00:6c:3c:04"
+#   cloudinit_path = "${path.module}/cloud_init_vm.cfg"
+# }
+
+
