f7b1a2778472017d95abc7e6bdc3e32549589d2b
mournfully <minuramolligoda.dev@gmail.com>
2022-12-04 21:57:49 -0500

    major rebase
    - removed files generated at runtime by terraform from git repo
    - overhauled directory structure into modules/ and scripts/
    - cleaned up remnants of last experiment

--- main.tf
+++ main.tf
@@ -14,17 +14,41 @@ provider "libvirt" {
 
 # create infrastructure modules
 module "network_1" {
-  source = "./modules/network"
+  source = "./modules/virtual-network"
 }
 
-module "testbench_1" {
+module "app_1" {
   source = "./modules/virtual-machine"
-  # set variables for module
-  vm_name = "testbench_1"
+  vm_name = "app_1"
   net_name = "network_1"
   mac_addr = "52:54:00:6c:3c:02" 
-  cloudinit_path = "${path.module}/cloud_init_test.cfg"
+  cloudinit_path = "${path.module}/scripts/local-ansible-testbench.yml"
 }
+
+module "app_2" {
+  source = "./modules/virtual-machine"
+  vm_name = "app_2"
+  net_name = "network_1"
+  mac_addr = "52:54:00:6c:3c:03" 
+  cloudinit_path = "${path.module}/scripts/local-ansible-testbench.yml"
+}
+
+module "db_1" {
+  source = "./modules/virtual-machine"
+  vm_name = "db_1"
+  net_name = "network_1"
+  mac_addr = "52:54:00:6c:3c:04" 
+  cloudinit_path = "${path.module}/scripts/local-ansible-testbench.yml"
+}
+
+# module "testbench_1" {
+#   source = "./modules/virtual-machine"
+#   # set variables for module
+#   vm_name = "testbench_1"
+#   net_name = "network_1"
+#   mac_addr = "52:54:00:6c:3c:02" 
+#   cloudinit_path = "${path.module}/cloud_init_test.cfg"
+# }
 
 # module "loadbalancer_1" {
 #   source = "./modules/virtual-machine"
--- modules/virtual-machine/virtual-machine.tf
+++ modules/virtual-machine/virtual-machine.tf
@@ -14,15 +14,24 @@ provider "libvirt" {
 
 # initialize variables
 variable "vm_name" {
+    type = string
+    description = "is used to set vm hostname and is how terraform identifies this resource"
 }
 
 variable "net_name" {
+    type = string
+    default = "network_1"
+    description = "doesn't do anything as terraform isn't managing the virtual network"
 }
 
 variable "mac_addr" {
+    type = string
+    description = "static addresses are assigned based on mac address by ./modules/virtual-network/config.xml"
 }
 
 variable "cloudinit_path" {
+    type = string
+    description = "sets path to any cloud_init.cfg in ./scripts/"
 }
 
 # create pool
@@ -36,36 +45,41 @@ resource "libvirt_pool" "resource_pool" {
 resource "libvirt_volume" "resource_image" {
     name = "${var.vm_name}_image.qcow2"
     pool = libvirt_pool.resource_pool.name
-    source = "${path.module}/../../downloads/debian-11-generic-amd64-20220911-1135.qcow2"
+    source = "${path.module}/../../downloads/debian-11-genericcloud-amd64.qcow2"
     format = "qcow2"    
 }
 
 # add cloudinit disk to pool
-resource "libvirt_cloudinit_disk" "resource_disk" {
+resource "libvirt_cloudinit_disk" "commoninit" {
     name = "commoninit.iso"
     pool = libvirt_pool.resource_pool.name
-    user_data = data.template_file.data_cloudinit.rendered
+    user_data = data.template_file.user_data.rendered
 }
 
 # read cloudinit configuration
-data "template_file" "data_cloudinit" {
+data "template_file" "user_data" {
     template = file("${var.cloudinit_path}")
+    # pass variables from terraform into cloud-init
+    vars = {
+      hostname = "${var.vm_name}"
+    }
 }
 
 # define kvm domain
 resource "libvirt_domain" "resource_domain" {
     # name must be unique
     name = "${var.vm_name}_domain"
-    memory = "1024"
+    memory = "512"
     vcpu = 1
     # add cloud init disk to share user data
-    cloudinit = libvirt_cloudinit_disk.resource_disk.id 
+    cloudinit = libvirt_cloudinit_disk.commoninit.id 
 
     # set to use default libvirt network
     network_interface {
         # set network_name here to force vm into desired v-net
         network_name = "${var.net_name}"
         mac = "${var.mac_addr}"
+        wait_for_lease = true
     }
 
     console {
@@ -83,4 +97,8 @@ resource "libvirt_domain" "resource_domain" {
         listen_type = "address"
         autoport = true
     }
+}
+
+output "IPs" {
+  value = libvirt_domain.resource_domain.*.network_interface.0.addresses
 }
